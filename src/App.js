698952716faac1b7c34238f185903d1a907af161
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-11-18 18:03:13 -0300

    feature: Compress Forms on PokedexForm, API Import and APP function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import './App.css';
-import StrenghtForm from './PokedexForm/StrengthForm';
-import NameForm from './PokedexForm/NameForm';
-import DefenseForm from './PokedexForm/DefenseForm';
-import WeaknesseForm from './PokedexForm/WeaknesseForm';
+import PokedexForm from './PokedexForm/PokedexForm';
+import React, { useEffect } from 'react';
+import { getPokemons, postPokemon, deletePokemon, putPokemon } from './api/api';
 
 function App() {
+
+  // Estado para armazenar as Informacoes do BackEnd
+  const [pokemons, setPokemons] = React.useState([])
+
+  // Estadi para Atualizar a lista.
+  const [pokemonUpdate, setPokemonUpdate] = React.useState(null)
+
+  // Carrega as Informacoes do BackEnd.
+  const loadPokemons = async () => {
+
+    const response = await getPokemons()
+    console.log("LOADPOKEMONS: ", response)
+
+    setPokemons(response)
+  }
+
+  console.log(pokemons)
+
+
+  // Funcao useEffect que carrega os Pokemons da Pokedex. 
+  React.useEffect(() => {
+    loadPokemons()
+  }, [])
+
+  const handleSetPokemon = (pokemon) => {
+
+    if (pokemonUpdate) {
+      putPokemon(pokemon, loadPokemons)
+    } else {
+      postPokemon(pokemon, loadPokemons)
+    }
+
+    setPokemonUpdate(null)
+  }
+
   return (
     <div className="App">
 
       <div className="formContainer">
         <h1>POKEDEX</h1>
-        <NameForm />
-        <StrenghtForm />
-        <DefenseForm />
-        <WeaknesseForm />
 
-        <button type="button" className="buttonOK">OK</button>
+        <PokedexForm handleSetPokemon={handleSetPokemon} pokemon={pokemonUpdate} />
+
       </div>
+
     </div >
   );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -11,3 +11,45 @@ export const getPokemons = async () => {
         console.log(e.message)
     }
 }
+
+// Funcao criar Pokemon.
+export const postPokemon = async (body, loadPokemons) => {
+    try {
+
+        await axios.post("http://localhost:3000/pokemon", body)
+
+        if (loadPokemons) loadPokemons();
+
+    } catch (e) {
+        console.log(e.message)
+    }
+
+}
+
+// Funcao que Deletea pokemon pelo ID.
+export const deletePokemon = async (id, loadPokemons) => {
+    try {
+
+        await axios.post("http://localhost:3000/pokemon/" + id)
+
+        if (loadPokemons) loadPokemons();
+
+    } catch (e) {
+        console.log(e.message)
+    }
+
+}
+
+// Funcao que Atualiza informacoes do Pokemon.
+export const putPokemon = async (body, loadPokemons) => {
+    try {
+
+        await axios.post("http://localhost:3000/pokemon/" + body.id, body)
+
+        if (loadPokemons) loadPokemons();
+
+    } catch (e) {
+        console.log(e.message)
+    }
+
+}
